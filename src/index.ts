c79f9a7e0ba1834b88d6bb7c3cb13e853487cdbc
Jeswin Kumar <jeswinpk@agilehead.com>
2018-07-17 14:43:21 +0530

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,3 @@
-/*
-  Supported urls
-  --------------
-  /
-  /posts
-  /projects
-  /events
-  /resume
-  /todos
-  /lessons
-
-  Url rewrites
-  ------------
-  Before getting called, paths are rewritten with the rewrite middleware.
-  So, /jeswin/posts/hello -> /posts/hello
-  with ctx.username = "jeswin"
-*/
-
 export default function(host: any) {
-  host.register([
-    ["/", "GET", home.index],
-    /* posts */
-    ["/posts", "GET", posts.index],
-    ["/posts/date/:date", "GET", posts.getByDate],
-    ["/posts/:slug", "GET", posts.get],
-    ["/categories/:id", "GET", posts.getCategory],
-    /* projects */
-    ["/projects", "GET", projects.index],
-    /* resume */
-    ["/resume", "GET", resume.index]
-  ]);
+  
 }
